dc7295272768cbf0050eb959b7d46928a7f8745a
dev-ggomes <gomesguilherme.pt@gmail.com>
2025-07-01 10:25:49 +0100

    creating: models.py

---
+++ src/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Float, Date
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+    id = Column(Integer, primary_key=True)
+    date = Column(Date, nullable=False)
+    type = Column(String, nullable=False)
+    category = Column(String, nullable=False)
+    amount = Column(Float, nullable=False)
+    description = Column(String)
