b7285618397c393f851d1bbaa96b3b5fae5cc4f8
dev-ggomes <gomesguilherme.pt@gmail.com>
2025-07-01 16:15:33 +0100

    add

--- source/views/dashboard.py
+++ source/views/dashboard.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from source.db import Session
-from source.utils import add_transaction, get_transactions, delete_transaction, get_monthly_report
+from source.utils import add_transaction, get_transactions, delete_transaction, get_monthly_report, get_categories
 from datetime import datetime
 
 bp = Blueprint('dashboard', __name__)
@@ -8,6 +8,7 @@ bp = Blueprint('dashboard', __name__)
 @bp.route('/', methods=['GET', 'POST'])
 def index():
     session = Session()
+    cats=get_categories(session)
     if request.method == 'POST':
         date_str = request.form['date']
         txn_date = datetime.strptime(date_str, '%Y-%m-%d').date()
@@ -15,14 +16,14 @@ def index():
             session,
             txn_date,
             request.form['type'],
-            request.form['category'],
+            int(request.form['category_id']),
             float(request.form['amount']),
             request.form.get('description')
         )
         return redirect(url_for('dashboard.index'))
 
     txns = get_transactions(session)
-    return render_template('dashboard.html', transactions=txns)
+    return render_template('dashboard.html', transactions=txns, categories=cats)
 
 @bp.route('/delete/<int:txn_id>', methods=['POST'])
 def delete(txn_id):
