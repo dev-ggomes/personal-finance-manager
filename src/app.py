7e22c6e6b5d0fa3be1d673da2f2110df844e791e
dev-ggomes <gomesguilherme.pt@gmail.com>
2025-07-01 11:05:55 +0100

    creating: app.py

---
+++ src/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from db import engine
+from models import Base
+
+# Inicializa o app Flask
+def create_app():
+    app = Flask(__name__, template_folder='templates')
+
+    # Garante que a DB existe
+    Base.metadata.create_all(engine)
+
+    # Regista blueprints
+    from views.dashboard import bp as dashboard_bp
+    app.register_blueprint(dashboard_bp)
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
