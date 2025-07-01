12acfcc0c90995fa17e7abcaeb27c545d282c7a2
dev-ggomes <gomesguilherme.pt@gmail.com>
2025-07-01 14:40:23 +0100

    add

--- source/app.py
+++ source/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from source.assets import init_assets
 from source.db import engine
 from source.models import Base
 
-# Inicializa o app Flask
+# Cria e configura a app
+
 def create_app():
-    app = Flask(__name__, template_folder='templates')
+    app = Flask(__name__, template_folder='templates', static_folder='static')
+
+    # Inicializa assets (SCSS -> CSS)
+    init_assets(app)
 
     # Garante que o DB existe
     Base.metadata.create_all(engine)
@@ -14,7 +19,6 @@ def create_app():
     app.register_blueprint(dashboard_bp)
 
     return app
-
 
 if __name__ == '__main__':
     app = create_app()
